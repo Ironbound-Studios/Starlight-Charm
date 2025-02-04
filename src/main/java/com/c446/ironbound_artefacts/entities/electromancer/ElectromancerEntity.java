860a6d34d8f09db02d48472fcb3601c0e8b08217
clcment446 <125682659+clcment446@users.noreply.github.com>
2025-02-04 11:54:55 +0100

    electromancer stuff

---
+++ src/main/java/com/c446/ironbound_artefacts/entities/electromancer/ElectromancerEntity.java
@@ -0,0 +1,46 @@
+package com.c446.ironbound_artefacts.entities.electromancer;
+
+import io.redspace.ironsspellbooks.api.registry.SpellRegistry;
+import io.redspace.ironsspellbooks.api.spells.AbstractSpell;
+import io.redspace.ironsspellbooks.entity.mobs.abstract_spell_casting_mob.AbstractSpellCastingMob;
+import io.redspace.ironsspellbooks.entity.mobs.goals.PatrolNearLocationGoal;
+import io.redspace.ironsspellbooks.entity.mobs.goals.WizardAttackGoal;
+import io.redspace.ironsspellbooks.entity.mobs.goals.WizardRecoverGoal;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.PathfinderMob;
+import net.minecraft.world.entity.ai.goal.FloatGoal;
+import net.minecraft.world.entity.ai.goal.LookAtPlayerGoal;
+import net.minecraft.world.entity.ai.goal.target.HurtByTargetGoal;
+import net.minecraft.world.entity.ai.goal.target.NearestAttackableTargetGoal;
+import net.minecraft.world.entity.ai.goal.target.ResetUniversalAngerTargetGoal;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.Level;
+
+import java.util.List;
+
+public class ElectromancerEntity extends AbstractSpellCastingMob {
+    protected ElectromancerEntity(EntityType<? extends PathfinderMob> pEntityType, Level pLevel) {
+        super(pEntityType, pLevel);
+        this.xpReward = 100;
+    }
+
+    protected void registerGoals() {
+        this.goalSelector.addGoal(1, new FloatGoal(this));
+        this.goalSelector.addGoal(2, (new WizardAttackGoal(this, (double) 1.25F, 25, 50)).setSpells(
+                List.of(SpellRegistry.BALL_LIGHTNING_SPELL.get(),
+                        SpellRegistry.BALL_LIGHTNING_SPELL.get(),
+                        SpellRegistry.BALL_LIGHTNING_SPELL.get(),
+                        SpellRegistry.LIGHTNING_BOLT_SPELL.get(),
+                        SpellRegistry.LIGHTNING_LANCE_SPELL.get()),
+                List.of(),
+                List.of(
+                        SpellRegistry.BURNING_DASH_SPELL.get()),
+                List.of()
+        ).setDrinksPotions().setSpellQuality(0.75f,1f));
+        this.goalSelector.addGoal(3, new PatrolNearLocationGoal(this, 30.0F, (double) 0.75F));
+        this.goalSelector.addGoal(8, new LookAtPlayerGoal(this, Player.class, 8.0F));
+        this.goalSelector.addGoal(10, new WizardRecoverGoal(this));
+        this.targetSelector.addGoal(1, new HurtByTargetGoal(this, new Class[0]));
+        this.targetSelector.addGoal(3, new NearestAttackableTargetGoal<>(this, Player.class, 10, true, false, this::isHostileTowards));
+    }
+}
